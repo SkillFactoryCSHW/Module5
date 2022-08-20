4f631f44bb0ab6d3fb71cf75ce4712fbb877caa5
Bochanikita <bochanikita@yandex.ru>
2022-08-20 13:04:07 +0300

    Add project files.

---
+++ Task1/Program.cs
@@ -0,0 +1,10 @@
+﻿(string Name, string[] Dishes) User;
+Console.WriteLine("Please, enter the user name");
+User.Name = Console.ReadLine();
+
+User.Dishes = new string[5];
+for (int i = 0; i < User.Dishes.Length; i++)
+{
+    Console.WriteLine("Enter the dish N{0}", i + 1);
+    User.Dishes[i] = Console.ReadLine();
+}
