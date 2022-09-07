b9922324c15f538bc48056c18ede0f9317b9a5c9
Bochanikita <bochanikita@yandex.ru>
2022-09-07 21:39:14 +0300

    Add task 5.1.6

---
+++ Task5.1.5/Program.cs
@@ -0,0 +1,21 @@
+﻿string[] colorArray = new string[3];
+for (int i = 0; i < colorArray.Length; i++)
+{
+    colorArray[i] = ShowColor(i);
+}
+ShowArray(colorArray);
+
+static void ShowArray(string[] colorArray)
+{
+    for (int i = 0; i < colorArray.Length; i++)
+    {
+        Console.WriteLine($"Your favorite color N{i + 1} is {colorArray[i]}");
+    }
+}
+
+static string ShowColor(int i)
+{
+    Console.WriteLine("Enter your favorite color N{0}", i + 1);
+    string favColor = Console.ReadLine();
+    return favColor;
+}
