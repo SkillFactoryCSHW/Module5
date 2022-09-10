97b79670212d666c6b53435d9a1bd12df2dd1fae
Bochanikita <bochanikita@yandex.ru>
2022-09-10 10:51:48 +0300

    Add task 5.2.17

---
+++ Task5.2.17/Program.cs
@@ -0,0 +1,30 @@
+﻿int[] array = { 3, 1, 4, 2, 5 };
+bool sortarray = true;
+ShowArray(array, sortarray);
+
+static void ShowArray(int[] array, bool sortarray = false)
+{
+    if(sortarray)
+    {
+        int temp;
+        for (int j = 0; j < array.Length; j++)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i + 1] < array[i])
+                {
+                    temp = array[i + 1];
+                    array[i + 1] = array[i];
+                    array[i] = temp;
+                }
+            }
+        }
+    }
+    else
+    {
+        for (int i = 0; i < array.Length; i++)
+        {
+            Console.Write(array[i] + " ");
+        }
+    }
+}
