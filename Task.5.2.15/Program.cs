5cd0a41b6b18bcef7c0fc7e0ef3b983262ad2c12
Bochanikita <bochanikita@yandex.ru>
2022-09-10 10:42:09 +0300

    Add task 5.2.15

---
+++ Task.5.2.15/Program.cs
@@ -0,0 +1,42 @@
+﻿int[] array = GetArrayFromConsole();
+SortArray(array);
+ShowArray(array);
+
+static int[] GetArrayFromConsole()
+{
+    int[] array = new int[5];
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.WriteLine("Введите элемент массива номер {0}", i + 1);
+        array[i] = int.Parse(Console.ReadLine());
+    }
+
+    return array;
+}
+
+static int[] SortArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        for (int j = 0; j < array.Length - 1; j++)
+        {
+            if (array[j + 1] < array[j])
+            {
+                int temp = array[j + 1];
+                array[j + 1] = array[j];
+                array[j] = temp;
+            }
+        }
+    }
+    int[] sortedarray = array;
+    return sortedarray;
+}
+
+static void ShowArray(int[] sortedarray)
+{
+    foreach (int i in sortedarray)
+    {
+        Console.Write(i + " ");
+    }
+}
